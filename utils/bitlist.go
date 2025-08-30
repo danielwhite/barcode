5e32f439ed67ef853e144ebcac7c473f27b38777
Daniel White <daniel@whitehouse.id.au>
2025-08-31 02:00:51 +1000

    utils: BitList backed by word size matching architecture

--- utils/bitlist.go
+++ utils/bitlist.go
@@ -1,11 +1,23 @@
 package utils
 
-import "math/bits"
+import (
+	"encoding/binary"
+	"math/bits"
+)
+
+const (
+	// wordSize of a word in the bit list data
+	wordSize = bits.UintSize
+	// wordBytes is the bytes of a word in the bit list data
+	wordBytes = wordSize / 8
+	// wordMask is used for bit indexing a word
+	wordMask = wordSize - 1
+)
 
 // BitList is a list that contains bits
 type BitList struct {
 	count int
-	data  []int32
+	data  []uint
 }
 
 // NewBitList returns a new BitList with the given length
@@ -13,11 +25,7 @@ type BitList struct {
 func NewBitList(capacity int) *BitList {
 	bl := new(BitList)
 	bl.count = capacity
-	x := 0
-	if capacity%32 != 0 {
-		x = 1
-	}
-	bl.data = make([]int32, capacity/32+x)
+	bl.data = make([]uint, wordsRequired(capacity))
 	return bl
 }
 
@@ -34,7 +42,7 @@ func (bl *BitList) grow() {
 		growBy = 1024
 	}
 
-	nd := make([]int32, len(bl.data)+growBy)
+	nd := make([]uint, len(bl.data)+growBy)
 	copy(nd, bl.data)
 	bl.data = nd
 }
@@ -42,7 +50,7 @@ func (bl *BitList) grow() {
 // AddBit appends the given bits to the end of the list
 func (bl *BitList) AddBit(bits ...bool) {
 	for _, bit := range bits {
-		itmIndex := bl.count / 32
+		itmIndex := bl.count / wordSize
 		for itmIndex >= len(bl.data) {
 			bl.grow()
 		}
@@ -53,8 +61,8 @@ func (bl *BitList) AddBit(bits ...bool) {
 
 // SetBit sets the bit at the given index to the given value
 func (bl *BitList) SetBit(index int, value bool) {
-	itmIndex := index / 32
-	itmBitShift := 31 - (index % 32)
+	itmIndex := index / wordSize
+	itmBitShift := wordMask - (index % wordSize)
 	if value {
 		bl.data[itmIndex] = bl.data[itmIndex] | 1<<uint(itmBitShift)
 	} else {
@@ -64,9 +72,9 @@ func (bl *BitList) SetBit(index int, value bool) {
 
 // GetBit returns the bit at the given index
 func (bl *BitList) GetBit(index int) bool {
-	itmIndex := index / 32
-	itmBitShift := 31 - (index % 32)
-	return ((bl.data[itmIndex] >> uint(itmBitShift)) & 1) == 1
+	itmIndex := index / wordSize
+	itmBitShift := wordMask - (index % wordSize)
+	return (bl.data[itmIndex] & (1 << itmBitShift)) != 0
 }
 
 // AddByte appends all 8 bits of the given byte to the end of the list
@@ -83,18 +91,20 @@ func (bl *BitList) AddBits(b int, count byte) {
 	}
 }
 
-// GetBytes returns all bits of the BitList as a []byte
+// GetBytes returns all bits of the BitList as a []byte.
+//
+// Bits are ordered in each byte using MSb 0 bit numbering where the
+// MSB is first and LSB is last.
 func (bl *BitList) GetBytes() []byte {
-	len := bl.count >> 3
-	if (bl.count % 8) != 0 {
-		len++
+	result := make([]byte, 0, bytesRequired(bl.count))
+	for i := range bl.data {
+		if wordSize == 32 {
+			result = binary.BigEndian.AppendUint32(result, uint32(bl.data[i]))
+		} else {
+			result = binary.BigEndian.AppendUint64(result, uint64(bl.data[i]))
+		}
 	}
-	result := make([]byte, len)
-	for i := 0; i < len; i++ {
-		shift := (3 - (i % 4)) * 8
-		result[i] = (byte)((bl.data[i/4] >> uint(shift)) & 0xFF)
-	}
-	return result
+	return result[:bytesRequired(bl.count)]
 }
 
 // IterateBytes iterates through all bytes contained in the BitList
@@ -103,13 +113,13 @@ func (bl *BitList) IterateBytes() <-chan byte {
 
 	go func() {
 		c := bl.count
-		shift := 24
+		shift := (wordBytes - 1) * 8
 		i := 0
 		for c > 0 {
 			res <- byte((bl.data[i] >> uint(shift)) & 0xFF)
 			shift -= 8
 			if shift < 0 {
-				shift = 24
+				shift = (wordBytes - 1) * 8
 				i++
 			}
 			c -= 8
@@ -124,7 +134,17 @@ func (bl *BitList) IterateBytes() <-chan byte {
 func (bl *BitList) Count() int {
 	n := 0
 	for _, x := range bl.data {
-		n += bits.OnesCount32(uint32(x))
+		n += bits.OnesCount(x)
 	}
 	return n
+}
+
+// wordsRequired returns the minimum number of words required to store n bits.
+func wordsRequired(n int) int {
+	return (n + wordMask) / wordSize
+}
+
+// bytesRequired returns the minimum number of words required to store n bits.
+func bytesRequired(n int) int {
+	return (n + 7) / 8
 }
--- utils/bitlist_test.go
+++ utils/bitlist_test.go
@@ -1,9 +1,71 @@
 package utils
 
 import (
+	"bytes"
 	"math/rand/v2"
 	"testing"
 )
+
+func TestBitList_AddBit(t *testing.T) {
+	bits := [...]bool{
+		0: true,
+		1: true,
+		3: true,
+		7: true,
+		8: true,
+	}
+	bl := NewBitList(0)
+	bl.AddBit(bits[:]...)
+
+	for i, want := range bits {
+		if got := bl.GetBit(i); want != got {
+			t.Errorf("GetBit(%d) = %t, want %t", i, got, want)
+		}
+	}
+}
+
+func TestBitList_SetBit(t *testing.T) {
+	bl := NewBitList(wordSize * 10)
+
+	// Randomly flip bits, ensuring that the expected bit is toggled.
+	for n := 0; n < bl.Len()*10; n++ {
+		i := rand.IntN(bl.Len())
+		want := !bl.GetBit(i)
+		bl.SetBit(i, want)
+		if got := bl.GetBit(i); want != got {
+			t.Errorf("GetBit(%d) = %t, want %t", i, got, want)
+		}
+	}
+}
+
+func TestBitList_GetBytes(t *testing.T) {
+	want := []byte{1, 3, 5, 9, 13}
+
+	bl := NewBitList(0)
+	for _, x := range want {
+		bl.AddByte(x)
+	}
+
+	if got := bl.GetBytes(); !bytes.Equal(want, got) {
+		t.Errorf("GetBytes() = %b, want %b", got, want)
+	}
+}
+
+func TestBitList_IterateBytes(t *testing.T) {
+	data := []byte{1, 3, 5, 9, 13}
+	bl := NewBitList(0)
+	for _, x := range data {
+		bl.AddByte(x)
+	}
+
+	var got []byte
+	for x := range bl.IterateBytes() {
+		got = append(got, x)
+	}
+	if want := bl.GetBytes(); !bytes.Equal(want, got) {
+		t.Errorf("GetBytes() = %b\nwant: %b", got, want)
+	}
+}
 
 func TestBitList_Count(t *testing.T) {
 	// Random bit list with an uneven split of 1s and 0s that span
